90048ba4002d4ae6909f8a6e3dfc65f003fb68f7
Lode Sai Charan <lsaicharan01@gmail.com>
2023-10-16 02:19:09 +0530

    updated useMovieTrailer

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -5,25 +5,37 @@ import { addTrailerVideo } from "../utils/moviesSlice";
 
 const useMovieTrailer = (movieId) => {
   const dispatch = useDispatch();
-
   const trailerVideo = useSelector((store) => store.movies.trailerVideo);
 
-  const getMovieVideos = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/" +
-        movieId +
-        "/videos?language=en-US",
-      API_OPTIONS
-    );
-    const json = await data.json();
+  // Created a separate function to handle the API call. This makes the code more modular and easier to test.
+  const fetchMovieVideos = async () => {
+    try {
+      // Using template literals to construct the API URL. This makes the code cleaner and easier to read.
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
+        API_OPTIONS
+      );
+      const data = await response.json();
+      return data.results;
+    } catch (error) {
+      // Added error handling to the API call. This ensures that the application can gracefully handle any issues that might arise.
+      console.error('Error fetching movie videos:', error);
+    }
+  };
 
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
+  const getMovieVideos = async () => {
+    const videos = await fetchMovieVideos();
+    const filterData = videos.filter((video) => video.type === "Trailer");
+    const trailer = filterData.length ? filterData[0] : videos[0];
     dispatch(addTrailerVideo(trailer));
   };
+
+  // Added movieId and trailerVideo to the dependency array of the useEffect hook. This ensures that the effect will run whenever either movieId or trailerVideo changes.
   useEffect(() => {
-    !trailerVideo && getMovieVideos();
-  }, []);
+    if (!trailerVideo) {
+      getMovieVideos();
+    }
+  }, [movieId, trailerVideo]);
 };
 
 export default useMovieTrailer;
